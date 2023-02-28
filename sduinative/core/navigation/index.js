4c89a48dcb59c977773cf767a44c61068a41d753
Shan Dhiviyarajan <prashasoft@gmail.com>
2023-02-28 08:57:17 +0530

    react native icons

--- sduinative/core/navigation/index.js
+++ sduinative/core/navigation/index.js
@@ -1,27 +1,19 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import {
-  createDrawerNavigator,
-  DrawerContentScrollView,
-  DrawerItemList,
-  DrawerItem,
-} from '@react-navigation/drawer';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Dashboard from '../../components/screens/dashboard';
 import Details from '../../components/screens/details';
 
 
-
-
-
 export default function RootNavigation() {
-const Drawer = createDrawerNavigator();
+const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-        <Drawer.Navigator 
+        <Stack.Navigator 
         initialRouteName="dashboard">
-            <Drawer.Screen name="dashboard" component={Dashboard}/>
-            <Drawer.Screen name="details" component={Details}/>
-        </Drawer.Navigator>
+            <Stack.Screen name="dashboard" component={Dashboard}/>
+            <Stack.Screen name="details" component={Details}/>
+        </Stack.Navigator>
     </NavigationContainer>
   );
 }
